e9612a4e84b7db654f72e63b4a65953a4df0bd39
ShariarMiraj <shahriarmiraj116@gmail.com>
2022-02-09 11:15:59 +0600

    Lab4

---
+++ MID_LAB_TASK_04_PHP/1.php
@@ -0,0 +1,15 @@
+<?php
+
+    $lenght= 30;
+    $width = 60;
+    
+    $area = $lenght *$width;
+
+    $perimeter  = 2* ($lenght + $width);
+
+
+    echo " Area of the rectangle  ". $lenght. " And ".$width. " is ".$area. "<br>";
+    echo " Perimeter of the rectangle  ". $lenght. " And ".$width. " is ".$perimeter ;
+
+
+?>
